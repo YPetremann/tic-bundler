4e476b030ed2f60fc0a255377c0eb602e381f208
YPetremann <y.petremann@gmail.com>
2024-02-11 03:26:46 +0100

    Ignoring unecessary files

--- jest.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defaults } from "jest-config";
-/** @type {import('jest').Config} */
-const config = {
-  transform: {},
-  testMatch: ["**/*?(*.)+(spec|test).[tj]s?(x)"],
-  verbose: true,
-};
-
-export default config;
