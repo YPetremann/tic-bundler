c8400deccb7ec5d530e31c24b635367aa3c5cb6b
YPetremann <y.petremann@gmail.com>
2024-02-11 13:36:21 +0100

    Squirrel support

--- langs/nut.mjs
+++ langs/nut.mjs
@@ -1,22 +1,28 @@
 import GenericTransformer from "./GenericTransformer.mjs";
 
-const prelude = `do
-  local m,p={},{}
-  function load(name,module) m[name]=module end
-  function require(name)
-    if not p[name] and m[name] then m[name],p[name]=m[name](require),true end
-    return m[name]
-  end
-end
+const prelude = `local m = {};
+local p = {};
+function load(name, module) {
+  m[name] <- module;
+}
+function require(name) {
+  if (!(name in p) && (name in m)) {
+    m[name] <- m[name](require);
+    p[name] <- true;
+  }
+  return m[name];
+}
 
 `;
 
 class JSTransformer extends GenericTransformer {
   ReRequire = /require\("([^"]+)"\)/g;
   WrRequire = (pth) => `require("${pth}")`;
-  ReModule = /\nload\("([^"]+)",function\(require\)\n  ((?:.|\n)+?\n)end\)\n/gm;
-  ReAsset = /\n(-- <[A-Z0-9]+>\n(?:.|\n)*\n-- <\/[A-Z0-9]+>\n)/gm;
-  WrLoad = (pth, src) => `load("${pth}",function(require)\n  ${src}\nend)\n\n`;
+  ReModule =
+    /\nload\("([^"]+)", function \(require\) {\n  ((?:.|\n)+?\n)}\);\n/gm;
+  ReAsset = /\n(\/\/ <[A-Z0-9]+>\n(?:.|\n)*\n\/\/ <\/[A-Z0-9]+>\n)/gm;
+  WrLoad = (pth, src) =>
+    `load("${pth}", function (require) {\n  ${src}\n});\n\n`;
   WrPrelude = () => prelude;
 }
 
